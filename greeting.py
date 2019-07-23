8ad4f0062bac9c09eb63a13101d106c8b469dcf8
andrew21-meet <andrew21@meet.mit.edu>
2019-07-23 12:29:51 +0300

    Stopping here

---
+++ greeting.py
@@ -0,0 +1,7 @@
+name = input ("what is your name   ")
+
+print("Hello there, " + name.capitalize())
+print('Your name is ' + str(len(name)) + " letters long")
+print("the first letter of your name is " + name[0].capitalize())
+print("the last letter of your name is " + name[-1])
+print("missing letters " + name[1:-1])
